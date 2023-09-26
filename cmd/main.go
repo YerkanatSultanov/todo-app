b9f83ee9f3db5bc00a963901223703fb544888f3
ysultanov <erkanatsultanov1999@gmail.com>
2023-09-26 23:09:45 +0600

    add configs file

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,30 @@ package main
 import (
 	"github.com/YerkanatSultanov/todo-app"
 	"github.com/YerkanatSultanov/todo-app/pkg/handler"
+	"github.com/YerkanatSultanov/todo-app/pkg/repository"
+	"github.com/YerkanatSultanov/todo-app/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	if err := initConfigs(); err != nil {
+		log.Fatalf("error in config files: %s", err)
+	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
-	if err := srv.Run(":8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8080"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server %s", err.Error())
 	}
+}
+
+func initConfigs() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("configs")
+
+	return viper.ReadInConfig()
 }
